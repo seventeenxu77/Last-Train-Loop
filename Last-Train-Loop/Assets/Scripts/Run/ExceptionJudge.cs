3e873b92a45499ad75179f28b1cbbf7e69f55768
Lalala7057 <2900705766@qq.com>
2025-12-10 18:43:07 +0800

    test

---
+++ Last-Train-Loop/Assets/Scripts/Run/ExceptionJudge.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExceptionJudge : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool hasException = true;
+    GameObject player = GameObject.Find("Player");
+   
+}
